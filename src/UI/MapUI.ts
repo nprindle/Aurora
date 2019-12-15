dc54e0a6755873f868447140c95ed7a09b928a0e
Grace Rarer <gracerarer@gmail.com>
2019-12-15 12:18:50 -0500

    map rendering now uses two canvases

--- src/UI/MapUI.ts
+++ src/UI/MapUI.ts
@@ -7,20 +7,26 @@ import AbstractTile from "../world/AbstractTile";
 // class to manage the UI canvas that shows the map
 export default class MapUI {
     world: World;
-    canvas: HTMLCanvasElement;
-    tileScale: number = 100; // pixels per tile (before being stretched)
+    worldCanvas: HTMLCanvasElement; // canvas containing the whole world
+    viewCanvas: HTMLCanvasElement; // the canvas we actually show, containing a subset of the world
+
+    viewWidth: number = 8;
+    viewHeight: number = 5
+
+    tileScale: number = 128; // pixels per tile (before being stretched)
     highlightedCoordinates: [number, number] | null = null; // [x, y] grid coordinates of the selected tile, or null if no tile is selected
 
     constructor(world: World) {
         this.world = world;
-        this.canvas = UI.makeCanvas(this.world.width * this.tileScale, this.world.height * this.tileScale, ['map-canvas']);
 
-        this.rerenderFullMap();
+        this.worldCanvas = UI.makeCanvas(this.world.width * this.tileScale, this.world.height * this.tileScale);
+        this.viewCanvas = UI.makeCanvas(this.viewWidth * this.tileScale, this.viewHeight * this.tileScale, ['map-canvas']);
 
-        this.canvas.addEventListener('click', ev => {
+        this.viewCanvas.addEventListener('click', ev => {
     
-            let x = Math.floor((ev.pageX - this.canvas.offsetLeft) * (this.world.width / this.canvas.clientWidth));
-            let y = Math.floor((ev.pageY - this.canvas.offsetTop) * (this.world.height / this.canvas.clientHeight));
+            // TODO adjust when view coords are nonzero
+            let x = Math.floor((ev.pageX - this.viewCanvas.offsetLeft) * (this.world.width / this.viewCanvas.clientWidth));
+            let y = Math.floor((ev.pageY - this.viewCanvas.offsetTop) * (this.world.height / this.viewCanvas.clientHeight));
         
             let targetTile = this.world.getTileAtCoordinates(x, y);
             
@@ -28,36 +34,48 @@ export default class MapUI {
 
             //TODO update world screen sidebar
         });
+
+        this.rerenderFullMap();
     }
 
-    rerenderFullMap(): HTMLElement {
+    getViewCanvas() {
+        return this.viewCanvas;
+    }
+
+    updateViewCanvas() {
+        let context = this.viewCanvas.getContext('2d')!;
+        context.drawImage(this.worldCanvas, 0, 0, this.viewCanvas.width, this.viewCanvas.height);
+    }
+
+    rerenderFullMap() {
         this.world.getTiles().forEach(tile => {
             this.rerenderTile(tile);
         });
-        return this.canvas;
     }
 
     drawImageAtCoordinates(src: string, gridX: number, gridY: number) {
-        let context = this.canvas.getContext('2d')!;
+        let context = this.worldCanvas.getContext('2d')!;
         let image = new Image();
         image.onload = () => {
             context.drawImage(image, gridX * this.tileScale, gridY * this.tileScale, this.tileScale, this.tileScale);
+            this.updateViewCanvas();
         }
 
         image.src = src;
         console.log(`Draw ${src} at ${gridX}, ${gridY}`);
+        
     }
 
-    // redraws the given tile at its selected location, and returns the edited canvas
-    rerenderTile(tile: AbstractTile): HTMLElement {
+    // redraws the given tile at its selected location
+    rerenderTile(tile: AbstractTile) {
+        // draw tile
         this.drawImageAtCoordinates(tile.getImgSrc(), tile.xPosition, tile.yPosition);
 
+        // draw highlight icon
         if ([tile.xPosition, tile.yPosition] === this.highlightedCoordinates) {
             console.log("Redrawing selected");
             this.drawImageAtCoordinates("assets/ui/highlight.png", tile.xPosition, tile.yPosition);
         }
-
-        return this.canvas;
     }
 
     selectTile(tile: AbstractTile | null) {
@@ -73,7 +91,7 @@ export default class MapUI {
 
         // highlight new tile
         if (tile) {
-            console.log(`select ${tile?.xPosition}, ${tile?.yPosition}`);
+            console.log(`select ${tile.xPosition}, ${tile.yPosition}`);
             this.drawImageAtCoordinates("assets/ui/highlight.png", tile.xPosition, tile.yPosition);
             this.highlightedCoordinates = [tile.xPosition, tile.yPosition];
         } else {
