01128000aab7bcaa3f2b3a42d36e18d7d70cf74c
Grace Rarer <gracerarer@gmail.com>
2019-12-16 09:35:52 -0500

    refactor tile positions to use a GridPosition object instead of separate x and y variables

---
+++ src/world/GridCoordinates.ts
@@ -0,0 +1,10 @@
+// grid coordinates
+export default class GridCoordinates {
+    readonly x: number;
+    readonly y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
