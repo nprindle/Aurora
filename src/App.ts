e091c731e1dca902d483a4295a969d5fad70810b
Grace Rarer <gracerarer@gmail.com>
2019-12-19 16:36:45 -0500

    remove debug print accidentally left after resource refactor

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,8 @@
 import GameWindow from "./UI/GameWindow.js"
-import Resource from "./resources/Resource.js";
 
 // this is the entrypoint to the program.
 window.onload = function() {
     console.log("Script Loaded");
 
     GameWindow.showMainMenu();
-    console.log(Resource.values());
 }
