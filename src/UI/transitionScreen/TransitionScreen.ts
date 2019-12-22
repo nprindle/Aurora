fc9ab481fe3c62341566c3e21c340bbcd109bfed
Grace Rarer <gracerarer@gmail.com>
2019-12-22 15:24:43 -0500

    transition between turns

---
+++ src/UI/transitionScreen/TransitionScreen.ts
@@ -0,0 +1,29 @@
+import UI from "../UI.js";
+import GameWindow from "../GameWindow.js";
+
+// "loading" screen shown between turns
+export default class TransitionScreen {
+    private html: HTMLElement;
+    
+    constructor() {
+        this.html = UI.makeDiv();
+
+        UI.fillHTML(this.html, [
+            UI.makePara("Loading..."), // TODO animation
+        ]);
+    }
+
+    getHTML() {
+        return this.html;
+    }
+
+    revealButton() {
+        UI.fillHTML(this.html, [
+            UI.makeButton("Continue", () => {this.continueToNextTurn();})
+        ]);
+    }
+
+    private continueToNextTurn() {
+        GameWindow.showWorldScreen();
+    }
+}
