143701c071b7cdb17f1af632e45f69b79ad9ebdd
Grace Rarer <gracerarer@gmail.com>
2019-12-22 23:53:17 -0500

    resource conversions are applied in a specific order

--- src/resources/Conversion.ts
+++ src/resources/Conversion.ts
@@ -3,12 +3,26 @@ import Cost from "./Cost";
 // a conversion from input resources to output resources that can be performed between turns by a tile if its input respurces are available
 export default class Conversion {
 
+    private static nextNumber = 1;
+
+    /* conversions are sorted by priority to determine the order in which conversions are applied
+     * lower numbered conversions are applied first; conversions with no imputs are priority 0
+     */
+    public priority: number;
+
     constructor(
         public readonly inputs: Cost[],
         public readonly outputs: Cost[],
     ) {
         if (outputs.length == 0) {
             throw "Tried to create a conversion that has no outputs. This should never exist.";
+        }
+
+        if (inputs.length == 0) {
+            this.priority = 0;
+        } else {
+            this.priority = Conversion.nextNumber;
+            Conversion.nextNumber++;
         }
     }
 }
