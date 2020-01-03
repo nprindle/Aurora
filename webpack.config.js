758c2a844903a6883579888654dc5cb92b7f69e0
nprindle <nprindle18@gmail.com>
2020-01-02 23:53:52 -0500

    Add HTML/CSS/JS minification and inlining
    - Configure webpack to minify and inline HTML/CSS/JS into a single file
    
    - Clean redundant webpack outputs after minification
    
    - Fix module resolution to fix source-map errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,60 @@
 const path = require('path');
+
 const ClosurePlugin = require('closure-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './out/App.js',
+  entry: {
+    main: './out/App.js',
+    styles: './stylesheets/styles.css',
+  },
   mode: 'production',
   optimization: {
     minimizer: [
       new ClosurePlugin({ mode: 'STANDARD', platform: 'javascript' }, {}),
     ],
   },
-  output: {
-    path: path.resolve(__dirname, 'dist/out'),
-    filename: 'App.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [ MiniCssExtractPlugin.loader, "css-loader" ]
+      },
+      {
+        test: /\.html$/i,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
   },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+    new FixStyleOnlyEntriesPlugin(),
+    new OptimizeCssAssetsPlugin(),
+    new HtmlWebpackPlugin({
+      template: './template.html',
+      inject: true,
+      inlineSource: '.(js|css)$',
+    }),
+    new HtmlWebpackInlineSourcePlugin(),
+    new CleanWebpackPlugin({
+      protectWebpackAssets: false,
+      cleanAfterEveryBuildPatterns: [ "*.js", "*.css" ],
+    }),
+  ],
 };
