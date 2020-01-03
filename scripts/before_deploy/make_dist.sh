095f07aa749e6fe9e69f6a6795a0304723de81ad
nprindle <nprindle18@gmail.com>
2020-01-02 22:52:18 -0500

    Configure deploy to run minification when enabled
    Also creates travis environment variable to enable minification during
    deploy

--- scripts/before_deploy/make_dist.sh
+++ scripts/before_deploy/make_dist.sh
@@ -8,7 +8,14 @@ fi
 
 mkdir -p "$target"
 
-cp -r out "$target"
 cp -r assets "$target"
-cp -r stylesheets "$target"
-cp *.html "$target"
+
+if [ "$MINIFY" = true ]; then
+  npm run minify
+  cp dist/index.html "$target"
+else
+  cp -r out "$target"
+  cp -r stylesheets "$target"
+  cp index.html "$target"
+fi
+
