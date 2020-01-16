5b71ce6250e670e8010a810fffd4f875059f48c2
Grace Rarer <gracerarer@gmail.com>
2020-01-16 17:35:09 -0500

    add credits rendering and css

--- src/UI/menu/CreditsScreen.ts
+++ src/UI/menu/CreditsScreen.ts
@@ -1,13 +1,30 @@
 import UI from "../UI.js";
 import GameWindow from "../GameWindow.js";
 
+
+class CreditsEntry {
+    constructor(public name: string, public roles: string[]){}
+}
+
+const credits: CreditsEntry[] = [
+    new CreditsEntry("Grace Rarer", ["Team Lead", "Gameplay Programmer"]),
+    new CreditsEntry("Prindle", ["Tooling Programmer"]), // <3
+]
+
 export default class CreditsScreen {
     static render(): HTMLElement {
+
+        console.log(credits);
+        const nameParas: HTMLElement[] = credits.map(entry => UI.makePara(entry.name, ["credits-name"]));
+        const roleParas: HTMLElement[] = credits.map(entry => UI.makePara(entry.roles.join(", "), ["credits-roles"]));
+
         return UI.makeDivContaining([
             UI.makeHeader("Credits", 1),
             UI.makeDivContaining([
-                UI.makeButton("Back", () => GameWindow.showMainMenu())
-            ]),
-        ], []);
-    } 
+                UI.makeDivContaining(nameParas, ["credits-names-column"]),
+                UI.makeDivContaining(roleParas),
+            ], ["credits-container"]),
+            UI.makeButton("Back", () => GameWindow.showMainMenu()),
+        ], ["credits"]);
+    }
 }
