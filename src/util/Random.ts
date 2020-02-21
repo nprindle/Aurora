d17524dcef69822916668bbc92266f0dc30285dd
nprindle <nprindle18@gmail.com>
2020-02-21 15:19:21 -0500

    Refactor Wasteland random texture selection

--- src/util/Random.ts
+++ src/util/Random.ts
@@ -17,4 +17,21 @@ export namespace Random {
     export function fromArray<T>(arr: T[]): T {
         return arr[intBetween(0, arr.length)];
     }
+
+    // Randomly select an element from an array, with selection biased based on
+    // a weight for each element.
+    export function fromWeightedArray<T>(weightedElems: [number, T][]): T {
+        const totalWeight = weightedElems.map(e => e[0]).reduce((x, y) => x + y, 0);
+        let n = Math.random() * totalWeight;
+        for (const [weight, x] of weightedElems) {
+            if (n <= weight) {
+                return x;
+            } else {
+                n -= weight;
+            }
+        }
+        // Default to the last element in the rare case that something went
+        // wrong with floating point
+        return weightedElems[weightedElems.length - 1][1];
+    }
 }
