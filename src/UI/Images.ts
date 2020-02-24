406e63429a7cc6be1fcb0bbdc8e401984a4de182
Grace Rarer <gracerarer@gmail.com>
2020-02-24 12:21:57 -0500

    move tile textures to separate folders

--- src/UI/Images.ts
+++ src/UI/Images.ts
@@ -19,17 +19,17 @@ function makeImage(src: string): HTMLImageElement {
 export const HighlightSelectionImage = makeImage("assets/ui/highlight.png");
 
 // tile textures
-export const HabitatTexture = makeImage("assets/tiles/habitat.png");
-export const WastelandTexture1 = makeImage("assets/tiles/wasteland1.png");
-export const WastelandTexture2 = makeImage("assets/tiles/wasteland2.png");
-export const WastelandTexture3 = makeImage("assets/tiles/wasteland3.png");
-export const WastelandTexture4 = makeImage("assets/tiles/wasteland4.png");
-export const WastelandTexture5 = makeImage("assets/tiles/wasteland5.png");
-export const SolarPanelsTexture = makeImage("assets/tiles/solar_panels.png");
-export const MountainTexture = makeImage("assets/tiles/mountain.png");
-export const MiningFacilityTexture = makeImage("assets/tiles/mining_facility.png");
-export const LanderTexture = makeImage("assets/tiles/lander.png");
-export const GreenhouseTexture = makeImage("assets/tiles/greenhouse.png");
+export const HabitatTexture = makeImage("assets/tiles/colony/habitat.png");
+export const WastelandTexture1 = makeImage("assets/tiles/environment/wasteland1.png");
+export const WastelandTexture2 = makeImage("assets/tiles/environment/wasteland2.png");
+export const WastelandTexture3 = makeImage("assets/tiles/environment/wasteland3.png");
+export const WastelandTexture4 = makeImage("assets/tiles/environment/wasteland4.png");
+export const WastelandTexture5 = makeImage("assets/tiles/environment/wasteland5.png");
+export const SolarPanelsTexture = makeImage("assets/tiles/colony/solar_panels.png");
+export const MountainTexture = makeImage("assets/tiles/environment/mountain.png");
+export const MiningFacilityTexture = makeImage("assets/tiles/colony/mining_facility.png");
+export const LanderTexture = makeImage("assets/tiles/colony/lander.png");
+export const GreenhouseTexture = makeImage("assets/tiles/colony/greenhouse.png");
 
 export async function preloadImages(): Promise<HTMLImageElement[]> {
     return Promise.all(imageQueue);
