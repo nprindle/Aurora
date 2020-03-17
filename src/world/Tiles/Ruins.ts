578d1db7ea2dfbcdd800af1769d8ddaf8c5ab5e8
Grace Rarer <gracerarer@gmail.com>
2020-03-16 23:42:18 -0400

    add alien ruins recycler

--- src/world/Tiles/Ruins.ts
+++ src/world/Tiles/Ruins.ts
@@ -2,16 +2,32 @@ import Tile from "../Tile.js";
 import GridCoordinates from "../GridCoordinates.js";
 import { RuinsTexture1, RuinsTexture2 } from "../../UI/Images.js";
 import { Random } from "../../util/Random.js";
+import TileProject from "../../tileProjects/TileProject.js";
+import Game from "../../Game.js";
+import Recycler from "./Recycler.js";
+import { TechPredicate } from "../../predicates/WorldPredicates.js";
+import { SurveyTech } from "../../techtree/TechTree.js";
+import Cost from "../../resources/Cost.js";
+import Resource from "../../resources/Resource.js";
+import { adjacentToRoad } from "../../predicates/TilePredicates.js";
 
 export default class Ruins extends Tile {
-    protected texture: HTMLImageElement = Random.fromWeightedArray([
-        [1 / 2, RuinsTexture1],
-        [1 / 2, RuinsTexture2],
-    ]);
+
+    private textureVariant: 1 | 2 = Random.bool() ? 1 : 2;
+    protected texture: HTMLImageElement = RuinsTexture1;
 
     constructor(position: GridCoordinates) {
         super(position);
     }
+
+    possibleProjects = [
+        new TileProject("Excavate", "Construct a facility to harvest exotic alien resources from the ruins",
+            (position: GridCoordinates, game: Game) => game.world.placeTile(new Recycler(position, this.textureVariant)),
+            [new Cost(Resource.BuildingMaterials, 200), new Cost(Resource.Energy, 100)],
+            [adjacentToRoad],
+            [new TechPredicate(SurveyTech)],
+        )
+    ];
 
 
     static readonly tileName: string = "Alien Ruins";
@@ -21,5 +37,14 @@ export default class Ruins extends Tile {
     }
     getTileDescription(): string {
         return Ruins.tileDescription;
+    }
+
+    getTexture(): HTMLImageElement {
+        switch (this.textureVariant) {
+        case 1:
+            return RuinsTexture1;
+        case 2:
+            return RuinsTexture2;
+        }
     }
 }
