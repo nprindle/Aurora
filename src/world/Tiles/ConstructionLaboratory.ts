0c137627d9c6bf9c01cdf6e97fb96bcce4d096db
Grace Rarer <gracerarer@gmail.com>
2020-03-17 23:51:59 -0400

    add technologies to tech tree

--- src/world/Tiles/ConstructionLaboratory.ts
+++ src/world/Tiles/ConstructionLaboratory.ts
@@ -12,7 +12,7 @@ import AlignmentLab from "./AlignmentLab.js";
 import Wasteland from "./Wasteland.js";
 import { TileWithinDistancePredicate, SpeciesPopulationPredicate, adjacentToRoad } from "../../predicates/TilePredicates.js";
 import Ruins from "./Ruins.js";
-import { RationalityTech, StructureConstructionTech, SurveyTech } from "../../techtree/TechTree.js";
+import { StructureConstructionTech, SurveyTech, AiResearchTech } from "../../techtree/TechTree.js";
 import { TechPredicate } from "../../predicates/WorldPredicates.js";
 import Species from "../../resources/Species.js";
 import { constructionProject } from "../../tileProjects/TileProject.js";
@@ -53,7 +53,7 @@ export default class ConstructionLaboratory extends Tile {
         constructionProject(AlignmentLab,
             [new Cost(Resource.BuildingMaterials, 20), new Cost(Resource.Electronics, 40)],
             [new SpeciesPopulationPredicate(Species.Human, 200), adjacentToRoad],
-            [new TechPredicate(RationalityTech)],
+            [new TechPredicate(AiResearchTech)],
         ),
     ];
 
--- src/world/Tiles/PsychLab.ts
+++ src/world/Tiles/PsychLab.ts
@@ -4,6 +4,11 @@ import Resource from "../../resources/Resource.js";
 import Conversion from "../../resources/Conversion.js";
 import Cost from "../../resources/Cost.js";
 import { PsychLabTexture } from "../../UI/Images.js";
+import TileProject from "../../tileProjects/TileProject.js";
+import Game from "../../Game.js";
+import { TechPredicate } from "../../predicates/WorldPredicates.js";
+import { CognitiveBiasesTech, RationalityTech, AiResearchTech } from "../../techtree/TechTree.js";
+import { NotTilePredicate } from "../../predicates/TilePredicates.js";
 
 
 export default class PsychLab extends Tile {
@@ -19,6 +24,17 @@ export default class PsychLab extends Tile {
             [new Cost(Resource.PsychKnowledge, 10)],
             25,
         ),
+    ];
+
+    possibleProjects = [
+        new TileProject(
+            "AI Safety Research Proposal",
+            "A human researcher wants to start a project with the goal of aligning artificial intelligence to serve human goals. If approved, this line of research risks mission failure, since if the overseer allowed itself to be reprogrammed it would no longer pursue the mission of uncovering information about the aliens at all costs.",
+            (position: GridCoordinates, game: Game) => game.unlockTechnology(AiResearchTech),
+            [],
+            [new TechPredicate(RationalityTech)],
+            [new TechPredicate(CognitiveBiasesTech), new NotTilePredicate(new TechPredicate(AiResearchTech))]
+        )
     ];
 
     static readonly tileName: string = "Psychology Lab";
