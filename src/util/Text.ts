0618ef7b9d064ab58bae44314758e2b3064ab2ed
nprindle <nprindle18@gmail.com>
2020-03-18 02:38:24 -0400

    Add line continuations to stripIndent

--- src/util/Text.ts
+++ src/util/Text.ts
@@ -13,13 +13,23 @@ export function indentWithNBS(str: string): string {
  * This takes a template literal as an argument, called with the following
  * syntax:
  *
+ * <pre><code>
  *     let str = stripIndent`
- *         A multiline template string
- *         Each line may be indented
- *             This line will still be indented by four spaces`;
+ *         A multiline template string.
+ *
+ *         Each line may be indented.
+ *
+ *             This line will still be indented by four spaces, and
+ *             this line will continue from the previous line.
+ *     `;
+ * </code></pre>
  *
  * Regular multiline templates do not remove any indentation. This will process
  * the template and strip extra indentation from the beginning of each line.
+ *
+ * To make a newline, leave an intermediate blank line. Adjacent lines without
+ * an intermediate blank line will be treated as a line continuation, and a
+ * space will be inserted.
  */
 export function stripIndent(strings: TemplateStringsArray, ...placeholders: string[]): string {
     // Remove leading/trailing newlines and concatenate arguments, accounting
@@ -27,7 +37,7 @@ export function stripIndent(strings: TemplateStringsArray, ...placeholders: stri
     const str = strings
         .reduce((acc, s, i) => acc + s + (placeholders[i] || ""))
         .replace(/\t/, "    ")
-        .replace(/^(?:\r?\n)+|(?:\r?\n)+$/g, "");
+        .replace(/^(?:\r?\n)+|(?:\r?\n)+ *$/g, "");
     // Split into lines and find the indentation levels of all nonempty ones
     const lines = str.split(/\r?\n/);
     const indentLevels = lines
@@ -39,11 +49,11 @@ export function stripIndent(strings: TemplateStringsArray, ...placeholders: stri
         minIndent = indentLevels.reduce((min, len) => Math.min(min, len));
     }
     // Drop the minimum indent size from every line and join them back
-    // together
-    if (minIndent > 0) {
-        const strippedLines = lines.map(str => str.substring(minIndent));
-        return strippedLines.join("\n");
-    } else {
-        return lines.join("\n");
-    }
+    // together. Join all pairs of newlines into a single newline and remove all
+    // independent newlines.
+    const strippedLines = minIndent > 0 ? lines.map(str => str.substring(minIndent)) : lines;
+    return strippedLines
+        .join("\n") // rejoin the lines into a single string
+        .replace(/([^\n])\n([^\n])/g, "$1 $2") // replace lone newlines with a space
+        .replace(/\n(\n+)/g, "$1"); // more than one newline means no continuation
 }
