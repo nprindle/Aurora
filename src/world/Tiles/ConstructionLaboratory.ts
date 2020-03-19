71e2cbe4153b81e425f1c5e34277e39db8a7470e
Grace Rarer <gracerarer@gmail.com>
2020-03-18 23:37:15 -0400

    move AI research project to arcology and habitat tiles

--- src/world/Tiles/ConstructionLaboratory.ts
+++ src/world/Tiles/ConstructionLaboratory.ts
@@ -12,7 +12,7 @@ import AlignmentLab from "./AlignmentLab.js";
 import Wasteland from "./Wasteland.js";
 import { TileWithinDistancePredicate, SpeciesPopulationPredicate, adjacentToRoad } from "../../predicates/TilePredicates.js";
 import Ruins from "./Ruins.js";
-import { StructureConstructionTech, SurveyTech, AiResearchTech } from "../../techtree/TechTree.js";
+import { SurveyTech, AiResearchTech } from "../../techtree/TechTree.js";
 import { TechPredicate } from "../../predicates/WorldPredicates.js";
 import Species from "../../resources/Species.js";
 import { constructionProject } from "../../tileProjects/TileProject.js";
