b981302fceb7803cd246a5bb72fa4b8c01b288a0
Grace Rarer <gracerarer@gmail.com>
2020-03-18 23:37:15 -0400

    move AI research project to arcology and habitat tiles

--- src/world/Tiles/Arcology.ts
+++ src/world/Tiles/Arcology.ts
@@ -3,12 +3,28 @@ import GridCoordinates from "../GridCoordinates.js";
 import Species from "../../resources/Species.js";
 import Housing from "../../resources/Housing.js";
 import { ArcologyTexture } from "../../UI/Images.js";
+import Game from "../../Game.js";
+import { AiResearchTech, RationalityTech, CognitiveBiasesTech } from "../../techtree/TechTree.js";
+import { TechPredicate } from "../../predicates/WorldPredicates.js";
+import { NotTilePredicate, SpeciesPopulationPredicate } from "../../predicates/TilePredicates.js";
+import TileProject from "../../tileProjects/TileProject.js";
 
 export default class Arcology extends Tile {
 
     protected texture: HTMLImageElement = ArcologyTexture;
 
     populationCapacity: Housing = new Housing(Species.Human, 2000);
+
+    possibleProjects = [
+        new TileProject(
+            "AI Safety Research Proposal",
+            "A human colonist wants to start a project with the goal of aligning artificial intelligence to serve human goals. If approved, this line of research risks mission failure, since if the overseer allowed itself to be reprogrammed it would no longer pursue the mission of uncovering information about the aliens at all costs.",
+            (position: GridCoordinates, game: Game) => game.unlockTechnology(AiResearchTech),
+            [],
+            [new TechPredicate(RationalityTech)],
+            [new TechPredicate(CognitiveBiasesTech), new NotTilePredicate(new TechPredicate(AiResearchTech)), new SpeciesPopulationPredicate(Species.Human, 500)]
+        )
+    ];
 
     constructor(position: GridCoordinates) {
         super(position);
