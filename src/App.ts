5b51fe58cf072e4c23a08aac8d3ca93b1644c001
nprindle <nprindle18@gmail.com>
2020-03-26 17:02:44 -0400

    Add main menu button to start/stop music

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,10 @@
 import { GameWindow } from "./UI/GameWindow.js";
 import { preloadImages } from "./UI/Images.js";
 import MainMenuUI from "./UI/menu/MainMenu.js";
-import { MusicManager } from "./music/MusicManager.js";
 
 // this is the entrypoint to the program.
 window.onload = async function() {
     console.log("Script Loaded");
     await preloadImages();
-    MusicManager.initialize();
     GameWindow.show(new MainMenuUI());
 };
