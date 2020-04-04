e6739d27e0006f757c8c9a371a72f5508c45f953
May Lawver <may@maycod.es>
2020-04-04 08:50:56 -0400

    Add Harmony of the Exospheres and Zeroball quotes

--- src/UI/transitionScreen/Quote.ts
+++ src/UI/transitionScreen/Quote.ts
@@ -246,7 +246,97 @@ export default class Quote {
             "An Oral History of The Lunar Republics"
         ),
 
+        new Quote(
+            stripIndent`The trend of musicologists funding interstellar colonies was a big shock to everybody, but in
+            retrospect they should've seen it coming. Anybody in that field would kill for the opportunity
+            to create a culture devoid of the European musical tradition's influence. The new perspectives
+            they'd bring would revitalize a field that had run out of interesting problems after the breakthrough
+            discovery of Cold Jazz Fusion.`,
+            "Harmony of the Exospheres by Caritas Wilson"
+        ),
+        new Quote(
+            stripIndent`Octave was the first musically motivated colony. While nobody knows how the Colonial Alliance
+            Department of Cultural Innovation got their hands on enough artificial wombs to birth Octave's initial wave of
+            colonists, it's obvious why that was necessary. If you want a colony of people with no preexisting ideas of what
+            music should be, you need people who haven't heard music before.`,
+            "Harmony of the Exospheres by Caritas Wilson"
+        ),
+        new Quote(
+            stripIndent`The first attempt at starting musical blank slate colony went well at first. The project nearly
+            failed when they received a shipment of Wagner operas meant for the Holy Bruderhof Kingdom. The operas were
+            stored on a long-dead magnetic tape format the colonists couldn't read, though, so disaster was averted.`,
+            "Harmony of the Exospheres by Caritas Wilson"
+        ),
+        new Quote(
+            stripIndent`Very few people know that the laws against broadcasting music between planets weren't the
+            music industry's idea. The musicologists at the Colonial Alliance Department of Cultural Innovation
+            were annoyed their experiments in creating new musical cultures kept getting ruined when colonists
+            overheard broadcasts from Interstellar Beats to Study To.`,
+            "Harmony of the Exospheres by Caritas Wilson"
+        ),
+        new Quote(
+            stripIndent`It's hard to believe now, but the set of people who got to experience zero-gravity was
+            a pretty exclusive club: astronauts, scientists, the rich and famous, the absurdly lucky.
+            Space elevators made sending people into orbit for "frivolous" reasons possible,
+            though, and once that happened, the stage was set for all sorts of recreational uses of zero-gravity.
+            It's no coincidence that the first Zeroball league was established less than a decade after the first
+            space elevator was built.`,
+            "Zeroball, Etc. by Parity Harker"
+        ),
+        new Quote(
+            stripIndent`Initially, weightless sports were all merely weightless versions of terrestrial sports.
+            Some fared better than others in the conversion. Most attempts to translate American football failed
+            because it's hard to have an intuition for how bodies behave in zero-gravity. A tackle leads to two
+            people sailing through the air until they hit a wall or another player. Hockey, which already has an
+            established underwater variant, failed for similar reasons. You can't push against the air as you can
+            with water, so once you're far from the walls, you're left doing your best bird impression in an attempt
+            to get somewhere.
 
+            The only sport which survived the conversion well was, funnily enough, bocce ball.`,
+            "Zeroball, Etc. by Parity Harker"
+        ),
+        new Quote(
+            stripIndent`
+            Our species is much safer than when our ancestors lived on only one planet. If we had been unlucky, all of humanity
+            could have been killed by one rogue asteroid, supervolcano, or gamma ray burst before ever reaching the stars. If a
+            planetary-scale disaster happened today it would be a tragedy, but humanity would still live on across dozens of major
+            populated worlds and hundreds of minor planets and stations.
+
+
+            Despite the safety benefits of an interstellar civilization, there are still existential risks. For example, a self-upgrading
+            general artificial intelligence would not be confined to one planet or star system. We should also be concerned about
+            militarized nanotechnology and biotechnology, as well as more speculative threats such as memetic weapons.
+            `,
+            "Journal of the Moretrue Institute for Effective Policy",
+        ),
+        new Quote(
+            stripIndent`
+            New Plymouth's Hall of the Founders was the first building of its size to be produced through structural flash-printing.
+            It was built by the Mayflower colonists as a short-term shelter, but they soon found that its high-density
+            construction would make demolishing the "temporary" structure prohibitively expensive. The dome still stands today,
+            serving as a museum and national monument. Despite the difficulty of demolishing or even renovating flash-printed
+            buildings once the ferrofoam has solidified, the speed and labor-saving make them an indispensible asset in the first
+            few years after planetfall.
+            `,
+            "Introduction to Frontier Engineering by Robin North",
+        ),
+        new Quote(
+            stripIndent`
+            The latest trend in computer automation is Neuromorphic Heuristic Intelligences, which are based on rough simulations
+            of the cognitive machinery of the human brain. NHIs are not qualitatively more intelligent than humans, which is why
+            the Colonial Alliance does not consider them to be prohibited under the San Jose Accords. However, they are quantitatively
+            more intelligent than humans, able to think faster and manage a large number of tasks while remaining completely focused
+            on their programmed objectives. Many experts believe that this is sufficient reason to restrict the use of this technology.
+            `,
+            "Dr. Alex Scott, Moretrue Institute for Effective Policy",
+        ),
+        new Quote(
+            stripIndent`
+            Kids who grew up after the terraforming don't realize how lucky they are. I remember when you couldn't take a step outside
+            Shackleton Base without a pressure suit. Every cubic meter of breathable air was a miracle.
+            `,
+            "An Oral History of The Lunar Republics"
+        ),
     ];
 
     static getRandomQuote(): Quote {
