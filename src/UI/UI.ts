5bbea7f24706e54240b81867601b9a81f19ea1f7
nprindle <nprindle18@gmail.com>
2020-04-11 14:32:33 -0400

    Extract makeTrackedSlider to UI

--- src/UI/UI.ts
+++ src/UI/UI.ts
@@ -119,6 +119,18 @@ export namespace UI {
         return div;
     }
 
+    // Make a slider with an adjacent label displaying its current value
+    export function makeTrackedSlider(
+        label: string, min: number, max: number, def: number, callback: (value: number) => void, step?: number
+    ): HTMLElement {
+        const tracker = UI.makePara(def.toString());
+        const slider = UI.makeSlider(label, min, max, def, (value: number) => {
+            callback(value);
+            tracker.innerText = value.toString();
+        }, step);
+        return UI.makeDivContaining([slider, tracker], ["settings-slider"]);
+    }
+
     // creates an HTML canvas for drawing graphics
     export function makeCanvas(width: number, height: number, classes?: string[]): HTMLCanvasElement {
         const canvas = document.createElement("canvas");
